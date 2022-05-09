139e23571ae76ffdd888be39d3197b0adc064fa0
MOGWAmogwa <79387913+MOGWAmogwa@users.noreply.github.com>
2022-05-09 11:01:28 +0900

    JS contact button scroll

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Make navbar transparent when it is on the top.
+
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
@@ -26,9 +27,21 @@ navbarMenu.addEventListener('click', (event) =>{
         return;
     } 
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior:'smooth'});
-
+  scrollIntoView(link);
 
 });
+
+
+// Handle click on "contact me" button on home
+
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact')
+})
+
+function scrollIntoView(selector){  
+    const scrollTo = document.querySelector(selector)
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+}
+
+

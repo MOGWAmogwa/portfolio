6c502716ddfec0921f6b6528f3cc1fed3b4d73bd
MOGWAmogwa <79387913+MOGWAmogwa@users.noreply.github.com>
2022-05-09 12:11:46 +0900

    JS Transparent

--- main.js
+++ main.js
@@ -45,3 +45,13 @@ function scrollIntoView(selector){
 }
 
 
+
+// Make home slowly fade to transparent as the window scrolls down
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity=1-window.scrollY /homeHeight 
+
+})
+
